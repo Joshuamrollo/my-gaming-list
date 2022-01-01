bc7f24457401d7c296807a2efc4a1e5fad1d9be7
Joshua Rollo <joshuamrollo@gmail.com>
2022-01-01 16:09:10 -0600

    final project

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -10,8 +10,7 @@ const User = require('../../models/User');
 
 router.post('/login', (req, res) => {
     const {email, password } = req.body;
-    console.log(email);
-    console.log(password);
+    
     if(!email || !password){
         return res.status(400).json({msg: 'Please enter all fields'})
     }
